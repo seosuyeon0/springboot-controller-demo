1a29b70b4bb0d620c8c18aaf14c8f39e0f05cafa
수연 <3437ssy@naver.com>
2023-12-08 16:56:40 +0900

    redirect

--- src/main/java/com/cos/controllerdemo/web/HttpRedirectionController.java
+++ src/main/java/com/cos/controllerdemo/web/HttpRedirectionController.java
@@ -8,13 +8,13 @@ public class HttpRedirectionController {
 
 	@GetMapping("/home")
 	public String home() {
-		// 1만줄 가정
+
 		return "home";
 	}
 	
 	@GetMapping("/away")
 	public String away() {
-		// 다른코드
-		return "redirect:/home"; // 리다이렉션이 된다. (@Controller)
+
+		return "redirect:/home";
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1> This is Home</h1>
+<h1> This is Home </h1>
 </body>
 </html>
